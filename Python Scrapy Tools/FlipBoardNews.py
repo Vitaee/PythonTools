a2c16b3e5f8200caeeebe868dd25a70212fd8041
Can İlgu <45064650+Vitaee@users.noreply.github.com>
2020-11-23 23:06:08 +0300

    will continue to be developed.

--- Python Scrapy Tools/FlipBoardNews.py
+++ Python Scrapy Tools/FlipBoardNews.py
@@ -1,5 +1,6 @@
-import requests
+import requests,time
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 class TechNews:
     def __init__(self):
@@ -62,16 +63,28 @@ class SportNews:
     def __init__(self):
         self.baseurl = "https://flipboard.com/section/spor-d7ilibva0mpg02ec"
         self.user_id = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}
+        
+        self.browser = webdriver.Chrome(executable_path=r"/home/vitae/chromedriver")
+        self.browser.get(self.baseurl)
+        time.sleep(2)
+        
+        i = 500
+        while i <= 1500:
+            self.browser.execute_script(f"window.scrollTo(0, {i})")
 
+            time.sleep(3)
+            i += 500
+
+        time.sleep(1)
+        self.data = self.browser.page_source
         self.NewsTitle = []
         self.NewsShortContent = []
         self.SourceWebSite = []
         self.NewsAvatar = []
 
     def VeriBelirle(self):
-        page = requests.get(self.baseurl, headers=self.user_id)
-        source = page.text
-        soup = BeautifulSoup(source, 'html.parser')
+        
+        soup = BeautifulSoup(self.data, 'html.parser')
 
         # Href belirle
         data = soup.find_all("h1", class_="post__title article-text--title--large")
